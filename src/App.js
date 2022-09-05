5d0093523f8d7b1b1d96d2fd827a29c84f6b4eb7
Daniela Trinchera <trinchera.dm@gmail.com>
2022-09-05 17:55:52 +0200

    working on 3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import "./App.css";
 import HomePage from "./components/HomePage";
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import ListBeers from "./components/ListBeers";
 import RandomBeer from "./components/RandomBeer";
 import NewBeer from "./components/NewBeer";
 import SingleBeer from "./components/SingleBeer";
 import OutletComponent from "./components/OutletComponent";
+import axios from "axios";
 
 function App() {
+  const [beersState, setBeersState] = useState([]);
+  const apiUrl = "https://ih-beers-api2.herokuapp.com/beers";
+
+  useEffect(() => {
+    axios
+      .get(apiUrl)
+      .then((response) => setBeersState(response.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log(beersState);
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route element={<OutletComponent />}>
-          <Route path="/beers" element={<ListBeers />} />
+          <Route
+            path="/beers"
+            element={<ListBeers beersState={beersState} />}
+          />
           <Route path="/random-beer" element={<RandomBeer />} />
           <Route path="/beers/:beerId" element={<SingleBeer />} />
           <Route path="/new-beer" element={<NewBeer />} />
--- src/components/ListBeers.jsx
+++ src/components/ListBeers.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function ListBeers() {
-  return <div>ListBeers</div>;
+function ListBeers({ beersState }) {
+  return (
+    <div>
+      {beersState.map((beer) => {
+        return (
+          <div key={beer._id}>
+            <Link className="one-beer" to={`/beers/${beer._id}`}>
+              <img
+                src={beer.image_url}
+                alt="one-beer-pic"
+                width={100}
+                className="one-beer-img"
+              />
+              {beer.name}
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default ListBeers;
